f344ff890641fd071c835b0808cda210ddd8c386
Anartz Mugika Ledo <mugan86@gmail.com>
2022-11-28 19:58:23 +0100

    feat: add CRUD Rest API Clients with nest g resource

--- a-books-api/src/app.module.ts
+++ a-books-api/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksService } from './books/books.service';
 import { BooksController } from './books/books.controller';
+import { ClientsModule } from './clients/clients.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, BooksController],
-  providers: [AppService, BooksService],
+  imports: [ClientsModule, BooksModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
