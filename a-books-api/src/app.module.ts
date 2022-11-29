f2b6b6625f2d4dc013044f680f23a35d3db83081
anartzdev <105705996+anartzdev@users.noreply.github.com>
2022-11-29 15:12:59 +0100

    feat: modules part1 - add book module and refactor

--- a-books-api/src/app.module.ts
+++ a-books-api/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksService } from './books/books.service';
-import { BooksController } from './books/books.controller';
-import { ClientsModule } from './clients/clients.module';
 import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [ClientsModule, BooksModule],
+  imports: [BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
