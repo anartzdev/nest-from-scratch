82d42ac2ece4aedb0a8a26ca226189af163f246e
anartzdev <105705996+anartzdev@users.noreply.github.com>
2022-12-01 10:37:51 +0100

    feat - add genres service and add in module genres

--- a-books-api/src/genres/genres.module.ts
+++ a-books-api/src/genres/genres.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GenresController } from './genres.controller';
+import { GenresService } from './genres.service';
 
 @Module({
-  controllers: [GenresController]
+  controllers: [GenresController],
+  providers: [GenresService]
 })
 export class GenresModule {}
